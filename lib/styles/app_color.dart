169b7efd5ad3e68ff7e7f1868ecf6de8afdba0c2
nguyenducmanh1N <manhn@gmail.com>
2025-06-29 15:29:52 +0700

    add app_color and app_text_styles

---
+++ lib/styles/app_color.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const Color uptodowhite = Color(0xffffffff);
+  static const Color uptodoblack = Color(0xff000000);
+
+  static const Color uptodoprimary_50 = Color.fromRGBO(134, 135, 231, 0.5);
+  static const Color uptodoprimary = Color.fromRGBO(134, 135, 231, 1.0);
+  static const Color uptodoBoder = Color.fromRGBO(151, 151, 151, 1);
+  static const Color uptodokeycolorsprimary = Color.fromRGBO(255, 255, 255, 0.87);
+
+  static const Color bgSecondary = Color.fromRGBO(54, 54, 54, 1);
+}
---
+++ lib/styles/app_text_styles.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+  static TextStyle uptododisplaylarge = TextStyle(
+    fontSize: 32,
+    decoration: TextDecoration.none,
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.w700,
+    letterSpacing: 0,
+  );
+
+  static const TextStyle spatzdisplaymedium = TextStyle(
+    fontSize: 45,
+    decoration: TextDecoration.none,
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.w400,
+    letterSpacing: 0,
+  );
+
+  static const TextStyle uptododisplaysmall = TextStyle(
+    fontSize: 16,
+    decoration: TextDecoration.none,
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.w400,
+    letterSpacing: 0,
+  );
+}
